943d3a55638ef088d913db0f052276cccf3c9670
SergioSilvaN <81711937+SergioSilvaN@users.noreply.github.com>
2021-04-08 19:04:25 -0500

    Update Minecraft.py

--- Minecraft.py
+++ Minecraft.py
@@ -1,33 +1,65 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 import random
-import datetime
-import math
-
+import serial as sr
+import threading
+gData=[]
+gData.append([0.0])
+gData.append([0.0])
 app = Ursina()
 score = 0
-time_past = int(datetime.datetime.now().second)
-level = 1
+abc=0
+ser=sr.Serial('COM3',19200)
+ser.reset_input_buffer
+a=0
+a1=0
+b=0
+p=0
+def getData(out_data):
+    while True:
+        
+        global a,b,p
+        ser.reset_input_buffer
+        line = ser.readline().decode('utf-8').replace('\r\n','').split(',')
+        
+      
+        try:
+            if(float(line[0])>0.000000001):
+            
+                a=float(line[0])/3000000
 
-p = 0
+            else:
+            
+                a=float(line[0])/300000
 
+            if(float(line[1])>0.000000001):
+            
+                b=float(line[1])/3000000
 
-def nivel():
-    global text_level, level
-    level += 1
-    text_level.text = 'Level '+str(level)
+            else:
+            
+                b=float(line[1])/300000
 
+            
+            player.vely=b
+            player.velx=a
+            p=float(line[2])
+            if(p==1 and a1==0):
+                p=1
+                a1=1
+            
+        
+        except:
+            pass
+dataCollector = threading.Thread(target=getData, args=(gData,))
+dataCollector.start()           
 
-def update():
-    global time_past
-    time = datetime.datetime.now().second-time_past
-    if time < 0:
-        time += 60
-    text_time.text = 'Time: '+str(time)
+   
 
 
 class Voxel(Button):
-    def __init__(self, position=(0, 0, 0), texture='descarga.jpeg'):
+
+    def __init__(self, position=(0, 0, 0), texture='chano.jpeg'):
         super().__init__(
             parent=scene,
             position=position,
@@ -36,15 +68,6 @@ class Voxel(Button):
             texture=texture,
             color=color.color(0, 0, random.uniform(0.9, 1)))
 
-class Cielo(Entity):
-    def __init__(self):
-        super().__init__(
-            parent=scene,
-            model= 'sphere',
-            texture='cielo.jpeg',
-            scale=150,
-            double_sided=True
-        )
 
 class Voxel_target(Button):
     def __init__(self, position=(0, 0, 0), texture='chano.jpeg'):
@@ -57,40 +80,26 @@ class Voxel_target(Button):
             color=color.color(0, 0, random.uniform(0.9, 1)))
 
     def input(self, key):
-        global score, time_past, p
+        global score,p
         if self.hovered:
-            if key == 'r':
+            if key == 'v':
                 destroy(self)
                 score += 1
-                text_score.text = 'Score: '+str(score)
-
-            if score == 10:
-                score = 0
-                text_score.text = 'Score: '+str(score)
-                time_past = datetime.datetime.now().second
-                for z in range(10):
-                    Voxel_target(position=(random.randint(-10, 10),
-                                 random.randint(1, 10), random.randint(0, 20)))
-                nivel()
+                text.text = 'Score: '+str(score)
+                Voxel_target(position=(random.randint(
+                    -10, 10), random.randint(1, 20), random.randint(0, 20)))
 
 
 for z in range(20):
     for x in range(-10,10):
         voxel = Voxel(position=(x, 0, z))
-
+ 
 for z in range(10):
     target = Voxel_target(position=(random.randint(
-        -10, 10), random.randint(1, 10), random.randint(0, 20)))
+        -10, 10), random.randint(1, 20), random.randint(0, 20)))
 
-text_score = Text('Score: '+str(score), y=.48, x=-.8, color=color.black)
-text_score.scale = (2, 2)
+text = Text('Score: '+str(score), y=.48, x=-.8)
+text.scale = (2, 2)
 
-text_time = Text('Time: '+str(time_past), y=.48, x=-.5, color=color.black)
-text_time.scale = (2, 2)
-
-text_level = Text(text='Level '+str(level), x=0, y=.48, color=color.black)
-text_level.scale = (2, 2)
-
-sky=Cielo()
-player = FirstPersonController(speed=10)
-app.run()
+player = FirstPersonController(speed=10,velx=a,vely=b)
+app.run(),
